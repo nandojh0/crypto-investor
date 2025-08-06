ca5348ea7e5175925051602ffde80351620d7747
nandojh0 <nandojh0@correo.com>
2025-08-05 21:57:53 -0500

    config proyect

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/crypto-investor/',
+  base: './',
   server: {
     proxy: {
       '/api': {
