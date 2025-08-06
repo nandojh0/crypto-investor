0a1c19fb4ba1e050342661537774d153eb9f097a
nandojh0 <nandojh0@correo.com>
2025-08-05 22:26:18 -0500

    config proyect 2

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: process.env.VITE_API_DOMINIO, // el backend
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
